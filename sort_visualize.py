dfe71f44746c73fcd313310e1fdea3a9e2be2099
Luke Eltiste <luke343279@gmail.com>
2022-03-23 13:28:02 -0700

    add comb, insertion and shell sort

--- sort_visualize.py
+++ sort_visualize.py
@@ -4,6 +4,7 @@ from time import sleep
 import threading
 from colour import Color
 
+
 pygame.init()
 WIDTH = 1000
 HEIGHT = 600
@@ -52,6 +53,7 @@ def merge(array, start, mid, end):
 
             while index != start:
                 array[index] = array[index - 1]
+                update()
                 index -= 1
             array[start] = value
             update()
@@ -102,6 +104,58 @@ def heap_sort(array):
         heapify(array, i, 0)
 
 
+def comb_sort(array):
+    length = len(array)
+    _gap = length
+    shrink = 1.3
+    sorted = False
+    while not sorted:
+        _gap /= shrink
+        gap = int(_gap)
+        if gap <= 1:
+            sorted = True
+            gap = 1
+        for i in range(length - gap):
+            sm = gap + i
+            if array[i] > array[sm]:
+                array[i], array[sm] = array[sm], array[i]
+                update()
+                sorted = False
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            update()
+            j -= 1
+        array[j + 1] = key
+        update()
+
+
+def shell_sort(array):
+    gap = len(array) // 2
+    while gap > 0:
+        i = 0
+        j = gap
+        while j < len(array):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+                update()
+            i += 1
+            j += 1
+
+            k = i
+            while k - gap > -1:
+                if array[k - gap] > array[k]:
+                    array[k - gap], array[k] = array[k], array[k - gap]
+                    update()
+                k -= 1
+        gap //= 2
+
+
 def update():
     screen.fill((255, 255, 255))
 
@@ -126,14 +180,18 @@ def update():
 
 def main():
     random.shuffle(array)
+    # array.reverse()
     update()
     sleep(1)
 
     # Uncomment the lines for the sorting algorithm you want to use
-    # threading.Thread(target=selection_sort, daemon=True, args=[array]).start()
-    # threading.Thread(target=merge_sort, daemon=True, args=[array]).start()
     # threading.Thread(target=bubble_sort, daemon=True, args=[array]).start()
-    threading.Thread(target=heap_sort, daemon=True, args=[array]).start()
+    # threading.Thread(target=comb_sort, daemon=True, args=[array]).start()
+    # threading.Thread(target=selection_sort, daemon=True, args=[array]).start()
+    # threading.Thread(target=insertion_sort, daemon=True, args=[array]).start()
+    # threading.Thread(target=heap_sort, daemon=True, args=[array]).start()
+    threading.Thread(target=shell_sort, daemon=True, args=[array]).start()
+    # threading.Thread(target=merge_sort, daemon=True, args=[array]).start()
 
     running = True
     while running:
